4cbb49d7056682e71e6188c8ef36872f9fab9ae4
Kevin Köster <kevin.koester@uni-hamburg.de>
2021-10-13 10:39:30 +0100

    Make libheaptrack header c compatible

--- src/track/libheaptrack.h
+++ src/track/libheaptrack.h
@@ -1,3 +1,5 @@
+#ifndef __libheaptrack_H__
+#define __libheaptrack_H__
 /*
     SPDX-FileCopyrightText: 2014-2017 Milian Wolff <mail@milianw.de>
 
@@ -10,21 +12,23 @@
 #ifdef __cplusplus
 typedef class LineWriter linewriter_t;
 extern "C" {
+typedef void (*heaptrack_callback_initialized_t)(linewriter_t&);
 #else
 typedef struct LineWriter linewriter_t;
+// FIXME: does this cause problems? & is not c compatible
+typedef void (*heaptrack_callback_initialized_t)(linewriter_t);
 #endif
 
-typedef void (*heaptrack_callback_t)();
-typedef void (*heaptrack_callback_initialized_t)(linewriter_t&);
+typedef void (*heaptrack_callback_t)(void);
 
 void heaptrack_init(const char* outputFileName, heaptrack_callback_t initCallbackBefore,
                     heaptrack_callback_initialized_t initCallbackAfter, heaptrack_callback_t stopCallback);
 
-void heaptrack_stop();
+void heaptrack_stop(void);
 
-void heaptrack_pause();
+void heaptrack_pause(void);
 
-void heaptrack_resume();
+void heaptrack_resume(void);
 
 void heaptrack_malloc(void* ptr, size_t size);
 
@@ -32,11 +36,12 @@ void heaptrack_free(void* ptr);
 
 void heaptrack_realloc(void* ptr_in, size_t size, void* ptr_out);
 
-void heaptrack_invalidate_module_cache();
+void heaptrack_invalidate_module_cache(void);
 
 typedef void (*heaptrack_warning_callback_t)(FILE*);
 void heaptrack_warning(heaptrack_warning_callback_t callback);
 
 #ifdef __cplusplus
 }
+#endif
 #endif
